3c10a912ddd03221e2a9869aebde8a814613736a
KevinMaze <kekeprojectphotmail.fr>
2023-04-07 16:21:42 +0200

    js begin game

---
+++ pendu/pendu.js
@@ -0,0 +1,10 @@
+const buttonPlay = document.getElementById("beginGame");
+const allWords = ['Fleur', 'Montagne', 'Plastique', 'Puéricultrice', 'Congolexicomatisation', 'Explosion']
+
+buttonPlay.addEventListener("click", function() {
+  beginGame()
+});
+
+function beginGame(){
+  // Générer un mot au hasard
+}
