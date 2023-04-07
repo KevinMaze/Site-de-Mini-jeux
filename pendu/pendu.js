26b416ebdb1297f1f6d1171b70997aa7d8ec2cd4
KevinMaze <kekeprojectphotmail.fr>
2023-04-07 16:52:32 +0200

    add function word

--- pendu/pendu.js
+++ pendu/pendu.js
@@ -1,10 +1,44 @@
-const buttonPlay = document.getElementById("beginGame");
-const allWords = ['Fleur', 'Montagne', 'Plastique', 'Puéricultrice', 'Congolexicomatisation', 'Explosion']
+// O Générer un mot aléa
+// - Afficher les lettres masquées  _ _ _ _ _
+// - Pouvoir proposer des lettres
+// - Afficher les lettres trouvées
+// - Gérer un nb d'erreur max
 
-buttonPlay.addEventListener("click", function() {
+const allWords = ['Fleur', 'Montagne', 'Plastique', 'Puéricultrice', 'Congolexicomatisation', 'Explosion', 'Macaron', 'Voiture', 'Pissenlit', 'Biroute']
+const buttonPlay = document.getElementById("beginGame");
+const worldToFindDiv = document.getElementById("worldToFindDiv");
+
+buttonPlay.addEventListener('click', function() {
   beginGame()
 });
 
 function beginGame(){
   // Générer un mot au hasard
+  worldToFindDiv.innerHTML = " "
+  let wordToFind = generateWord();
+  // worldToFindDiv.innerHTML = wordToFind;
+  let wordToFindArray = Array.from(wordToFind);
+
+  let table = document.createElement("table");
+  let line = document.createElement("tr");
+  wordToFindArray.forEach(letter => {
+    // Créer un TD (case du tableau) par lettre
+    let td = document.createElement("td");
+    td.dataset.letter = letter;
+    td.innerText = "_";
+    line.appendChild(td);
+  });
+  table.appendChild(line);
+  worldToFindDiv.appendChild(table);
 }
+
+function generateWord(){
+  let indexWorld = getRandomInt(allWords.length);
+  return allWords[indexWorld];
+}
+
+function getRandomInt(max){
+  return Math.floor(Math.random() * max);
+}
+
+// VIDEO A 32:00
