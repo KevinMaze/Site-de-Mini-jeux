3176187a1dd28a9193d0ada6d339542287980aed
Kévin MAZE <kekeproject@hotmail.fr>
2023-04-16 18:03:00 +0200

    add cpt and letter

--- pendu/pendu.js
+++ pendu/pendu.js
@@ -1,14 +1,19 @@
 // O Générer un mot aléa
 // O Afficher les lettres masquées  _ _ _ _ _
-// - Pouvoir proposer des lettres
-// - Afficher les lettres trouvées
-// - Gérer un nb d'erreur max
-// - Afficher des lettres visible
+// O Pouvoir proposer des lettres
+// O Afficher les lettres trouvées
+// O Gérer un nb d'erreur max
+// O Gérer la victoire
+// O Afficher des lettres visible (selon difficulté)
+// 
 
-const allWords = ['Fleur', 'Montagne', 'Plastique', 'Puéricultrice', 'Congolexicomatisation', 'Explosion', 'Macaron', 'Voiture', 'Pissenlit', 'Biroute']
+const allWords = ['fleur', 'montagne', 'plastique', 'puéricultrice', 'congolexicomatisation', 'explosion', 'macaron', 'voiture', 'pissenlit', 'biroute']
 const buttonPlay = document.getElementById("beginGame");
 const worldToFindDiv = document.getElementById("worldToFindDiv");
 const keyboardDiv = document.getElementById("keyboard");
+let wordToFind ='';
+let wordToFindArray;
+let cptErreur = 0;
 
 buttonPlay.addEventListener('click', function() {
   beginGame()
@@ -16,13 +21,15 @@ buttonPlay.addEventListener('click', function() {
 
 function beginGame(){
   // Générer un mot au hasard
+  cptErreur.innerHTML = 0;
   worldToFindDiv.innerHTML = " "
-  let wordToFind = generateWord();
+  wordToFind = generateWord();
   // worldToFindDiv.innerHTML = wordToFind;
-  let wordToFindArray = Array.from(wordToFind);
+  wordToFindArray = Array.from(wordToFind);
 
   let table = document.createElement("table");
   let line = document.createElement("tr");
+  line.id = "LineOfWord";
   wordToFindArray.forEach(letter => {
     // Créer un TD (case du tableau) par lettre
     let td = document.createElement("td");
@@ -43,6 +50,34 @@ function generateKeyboard(){
     lettreDiv.innerHTML = letter;
     lettreDiv.classList.add("letterKeyboard");
     keyboardDiv.appendChild(lettreDiv);
+
+    lettreDiv.addEventListener("click", () => {
+      if(checkLetterInWord(letter)){
+        // Afficher la lettre dans le mot masqué
+        let lineWord = document.getElementById("lineOfWord");
+        let allTdOfWord = lineWord.children;
+        Array.from(allTdOfWord).forEach(td => {
+          if(td.dataset.letter == letter){
+            td.innerHTML = letter;
+          }
+        });
+      }
+      else{
+        // Incrémenter le compteur d'erreur
+        cptErreur ++;
+        document.getElementById("cptErreur").innerHTML = cptErreur;
+        if(cptErreur >= 5){
+          document.getElementById("cptErreur").innerHTML = "Perdu, vous avez fait plus de 5 erreurs";
+          let lineWord = document.getElementById("lineOfWord");
+          let allTdOfWord = lineWord.children;
+          Array.from(allTdOfWord).forEach(td => {
+            td.innerHTML = td.dataset.letter;
+        });
+        keyboardDiv.innerHTML = '';
+        }
+      }
+      lettreDiv.style.visibility = "hidden"
+    })
   });
 }
 
@@ -69,4 +104,16 @@ function generateWord(){
 
 function getRandomInt(max){
   return Math.floor(Math.random() * max);
+}
+
+// Retourne true si la lettre est présente dans le mot
+// Retourne false si absente
+function checkLetterInWord(letter){
+  let findLettre = false;
+  wordToFindArray.forEach(letterOfWord => {
+    if(letter == letterOfWord){
+      findLettre = true;
+    }
+  });
+  return findLettre;
 }
