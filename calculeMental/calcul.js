cace098460339ab8197da2b36680cce630fa6142
Kévin MAZE <kekeproject@hotmail.fr>
2023-04-16 19:29:53 +0200

    add button submit

--- calculeMental/calcul.js
+++ calculeMental/calcul.js
@@ -4,15 +4,30 @@
 
 // V2 - Paramètrer ma partie, le compte a rebours, les opérateurs de la partie
 const reboursDiv = document.getElementById("minuteur")
+const calculDiv = document.getElementById("calcul")
+const propalInput = document.getElementById("resultPropal")
 const tempsMinuteurBase = 5
 let compteurInterval = null
 let tempsRestant = 0
+let calculEnCour = null
+
+document.getElementById("validePropal").addEventListener("click", () => {
+    if(propalInput.value == calculEnCour.result) {
+        alert("bravo")
+    }
+    else {
+        alert("C'est non")
+    }
+})
+
 
 function launchGame() {
     launchMinuteur(tempsMinuteurBase)
 }
 
 function generateCalcul() {
+    calculEnCour = new Calcul(500)
+    calculDiv.innerText = calculEnCour.showCalcul
 
 }
 
@@ -31,15 +46,36 @@ function launchMinuteur(tempsMinuteurBase) {
 }
 
 class Calcul {
+    #operators = ["*", "-", "+"]
     number1
     number2
     operator
-    result
-    constructor() {
-        let max = this.#getrandomInt()
-        this.number1 = max
-        this.number2 = max
+
+    constructor(maximum) {
+        this.number1 = this.#getrandomInt(maximum)
+        this.number2 = this.#getrandomInt(maximum)
+        this.operator = this.#operators[this.#getrandomInt(3)]
     }
+
+    get result() {
+        return eval(this.number1+this.operator+this.number2)
+    }
+
+    get showCalcul() {
+        return `${this.number1} ${this.operator} ${this.number2}`
+    }
+    // get result() {
+    //     if(this.operator == '-') {
+    //         return this.number1 - this.number2
+    //     }
+    //     else if(this.operator == '+') {
+    //         return this.number1 + this.number2
+    //     }
+    //     else if(this.operator == '*') {
+    //         return this.number1 * this.number2
+    //     }
+    // }
+
     #getrandomInt(max) {
         return Math.floor(Math.random() * max)
     }
