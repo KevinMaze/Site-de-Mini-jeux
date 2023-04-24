af85c054f261ab898010983ef1b4b7edae55f0d6
KevinMaze <kekeprojectphotmail.fr>
2023-04-24 17:03:27 +0200

    add confetti and choice color

--- mastermind/mastermind.js
+++ mastermind/mastermind.js
@@ -5,9 +5,10 @@
 -> Ajouter d'autres éléments....
 */
 import { Utils } from "../juste prix/lib/utils.js"
-
+import { confetti } from "../juste prix/lib/confetti.js"
 
 const colors = ["red", "blue", "yellow", "pink"]
+const allSelectDiv = document.getElementById("allSelect")
 let colorTabToFind = null
 const nbColorToFind = 4
 document.getElementById("start").addEventListener("click", () => {
@@ -18,13 +19,64 @@ document.getElementById("start").addEventListener("click", () => {
 
 function launchGame(){
   setAleaColorTab()
-  document.getElementById("allS")
-  for (let index = 0; index < nbColorToFind; index++){
-    generateSelect("allSelect")
-  }
+  allSelectDiv.innerHTML = ""
+  generateLigneSelect()
+  // console.log(colorTabToFind)
 }
 
-function generateSelect(idCible){
+function checkProposition(){
+  let allSelect = allSelectDiv.querySelectorAll("select")
+  let propal = Array.from(allSelect, select => select.value).slice(-4)
+  console.log(propal)
+
+  let cptGoodPlace = 0
+  let cptBadPlace = 0
+// parcours le tableau de proposition
+  for (let i = 0; i < propal.length; i++) {
+    // on compare les couleur
+    if(propal[i] == colorTabToFind[i]){
+      // la proposition est bonne (bonne couleur au bon endroit)
+      cptGoodPlace++
+    }
+    
+  }
+
+// ajout de la ligne de message de points
+  let lineResponse = document.createElement("div")
+  lineResponse.innerText = "Couleur(s) bien placées" + cptGoodPlace
+  allSelectDiv.appendChild(lineResponse)
+
+  // si on a autant de bonne réponse que de case dans tableau = win
+  if(cptGoodPlace == colorTabToFind.length){
+    confetti.launchAnimationConfetti();
+    let audio = new Audio("../juste prix/audio/oui.mp3");
+    audio.play();
+    setTimeout(() => {
+      confetti.stopAnimationConfeti();
+    }, 5000)
+  }
+
+  // on génère des nouveaux select
+  generateLigneSelect()
+}
+
+function generateLigneSelect(){
+  let line = document.createElement("div")
+  for (let index = 0; index < nbColorToFind; index++){
+    generateSelect(line)
+  }
+  // Générer un btn ok pour validé
+  let btn = document.createElement("button")
+  btn.innerText = "OK"
+  line.appendChild(btn)
+  btn.addEventListener("click", () => {
+    checkProposition()
+  })
+  allSelectDiv.appendChild(line)
+}
+
+
+function generateSelect(target){
   // création élément select
   let mySelect = document.createElement("select")
   colors.forEach(color => {
@@ -35,7 +87,7 @@ function generateSelect(idCible){
     colorOption.style.backgroundColor = color
     mySelect.appendChild(colorOption)
   });
-  document.getElementById(idCible).appendChild(mySelect)
+  target.appendChild(mySelect)
 }
 
 function setAleaColorTab(size = 4){
@@ -55,8 +107,7 @@ function getAleaColors(){
 
 
 
-
-
+// reste 34:41 min séance 8
 
 
 
