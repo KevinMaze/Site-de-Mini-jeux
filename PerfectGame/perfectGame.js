a5e9bffd5ab3b1718f20440bff405a68471af9c5
KevinMaze <kekeprojectphotmail.fr>
2023-04-26 15:46:12 +0200

    add perfect game

---
+++ PerfectGame/perfectGame.js
@@ -0,0 +1,7 @@
+// Déroulement 1 tour :
+// Démarer un partie
+// Générer carte aléa
+// Laisser carte visible pendant 5s
+// Laisser 3s au joueur pour choisir les cartes
+// Si gagner => recommencer le tour
+// Si perdu => fin de la partie
