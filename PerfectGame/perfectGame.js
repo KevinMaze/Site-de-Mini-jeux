a9eba86705cfa483b51c15a9772848df070569f9
KevinMaze <kekeprojectphotmail.fr>
2023-04-27 16:59:50 +0200

    add hidden cards

--- PerfectGame/perfectGame.js
+++ PerfectGame/perfectGame.js
@@ -1,8 +1,8 @@
 // Déroulement 1 tour :
 // Démarer un partie
-// Générer carte aléa
-// Laisser carte visible pendant 5s
-// Choisir une carte au hasard parmis les cartes tirées
+// Générer carte aléa   OK
+// Laisser carte visible pendant 5s  OK
+// Choisir une carte au hasard parmis les cartes tirées   OK
 // Laisser 3s au joueur pour choisir les cartes
 // Si gagner => recommencer le tour
 // Si perdu => fin de la partie
@@ -10,8 +10,45 @@
 import { Utils } from "../juste prix/lib/utils.js"
 
 const plateau = document.getElementById('cardsPlateau')
+const elementToFindDiv = document.getElementById('elementToFind')
+const nbCardsParam = 6
+const codeError = document.getElementById('codeErrorDiv')
+let classCardToFind
 
-generateCards(24)
+
+newTour()
+
+function newTour(){
+  plateau.innerHTML = ''
+  elementToFindDiv.innerHTML = ''
+  generateCards(nbCardsParam)
+  let nbCardToFind = Utils.getRandomInt(nbCardsParam)
+  let cardsPlateau = plateau.querySelectorAll('.perso')
+  classCardToFind = cardsPlateau[nbCardToFind].classList
+
+  setTimeout(() => {
+    let allCards = document.querySelectorAll('.perso')
+    allCards.forEach(card => {
+      card.classList.add('hidden')
+      card.addEventListener('click', (e) => {
+        if(classCardToFind.value == card.classList.value){
+          card.classList.remove('hidden')
+          setTimeout(() => {
+            newTour()
+          }, 3000)
+          
+        }
+        else {
+          codeError.innerHTML = 'Perdu'
+        }
+      })
+    })
+    let newCardToFind = document.createElement('div')
+    newCardToFind.classList = classCardToFind
+    newCardToFind.classList.remove('hidden')
+    elementToFindDiv.appendChild(newCardToFind)
+  }, 5000)
+}
 
 // Générer des cartes aléatoirement
 function generateCards(nbCards){
@@ -27,3 +64,7 @@ function generateCards(nbCards){
     plateau.appendChild(newCard)
   }
 }
+
+
+
+// reste 16:19m séance 11
