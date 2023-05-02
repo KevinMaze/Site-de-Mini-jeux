351d67433f4e04968e97d4fafe28176c8d1de4a9
KevinMaze <kekeprojectphotmail.fr>
2023-05-02 16:56:25 +0200

    add card and function

--- memory/memory.js
+++ memory/memory.js
@@ -1,17 +1,22 @@
-// import { Utils } from "../juste prix/lib/utils.js"
-
 let jeuTableau
 let allCards = document.querySelectorAll('.card')
 let cptClickCurrent = 0
-let dataImageShowed
+let cardClickedId
+const cards = ['king', 'queen', 'valet', 'as']
 
+// j'ajoute l'évènement du click sur toutes les cartes
 allCards.forEach(card => {
   card.addEventListener('click', () =>{
-    playGame(card)
+    clickOnCardEvent(card)
   })
 })
 
-function playGame(card){
+
+// fonction qui gère le click sur une carte
+function clickOnCardEvent(card){
+  if(card.classList.contains('finded')){
+    return
+  }
   cptClickCurrent++
 
   if(cptClickCurrent == 1){
@@ -29,23 +34,35 @@ function playGame(card){
     card.classList.remove('hidden')
     //je stocke la téponse derriere la carte
     //et je la retourne
-    dataImageShowed = card.dataset.image
+    cardClickedId = card.id
   }
 
   else if (cptClickCurrent == 2){
     //2eme click, je verifie si image à été trouver
-    card.classList.remove('hidden')
-    if(dataImageShowed == card.dataset.image){
-      allCards.forEach(card => {
-        if(card.classList.contains('hidden')){
-            //c'est une carte caché
-        }
-        else{
-          card.classList.add('finded')
-        }
-      })
+    if(cardClickedId == card.id){
+      cptClickCurrent = 1
     }
-    cptClickCurrent = 0
-    dataImageShowed = ''
+    else {
+      card.classList.remove('hidden')
+      let cardClickedBefore = document.getElementById(cardClickedId)
+      if(cardClickedBefore.dataset.image == card.dataset.image){
+        allCards.forEach(card => {
+          if(card.classList.contains('hidden')){
+              //c'est une carte caché
+          }
+          else{
+            card.classList.add('finded')
+          }
+        })
+      }
+      cptClickCurrent = 0
+      cardClickedId = ''
+    }
   }
 }
+
+function initGame(nbPaires){
+  for()
+}
+
+// seccion 3 reste 16:10
